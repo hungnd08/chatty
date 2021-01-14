e49730e493300a6a4de8de8e98d8802bf1eef9f0
ddhung <ddhung@ambition.vn>
2021-01-14 18:04:07 +0700

    css chat pages, display msg

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { auth } from '../services/firebase';
-import { db } from '../services/firebase';
+import React, { useRef, useEffect, useState } from 'react';
+import Header from "../components/Header";
+import { auth, db } from '../services/firebase';
 
 export default function Chat() {
     const [state, setState] = useState({
@@ -11,31 +11,76 @@ export default function Chat() {
         writeError: null
     });
 
-    useEffect(() => {
-        setState({ readError: null });
+    useEffect(async () => {
+        setState((prevProps) => ({
+            ...prevProps,
+            readError: null
+        }));
         try {
             db.ref("chats").on("value", snapshot => {
                 let chats = [];
                 snapshot.forEach((snap) => {
                     chats.push(snap.val());
                 });
-                setState({ chats });
+                setState((prevProps) => ({
+                    ...prevProps,
+                    chats
+                }));
             })
         } catch(error) {
-            setState({ readError: error.message });
+            setState((prevProps) => ({
+                ...prevProps,
+                readError: error.message
+            }));
         }
-    });
+    }, []);
+
+    function handleChange(event) {
+        setState((prevProps) => ({
+            ...prevProps,
+            content: event.target.value
+        }));
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setState((prevProps) => ({
+            ...prevProps,
+            writeError: null
+        }));
+        // const chatArea = 
+    }
+
+    function formatTime(timestamp) {
+        const d = new Date(timestamp);
+        const time = ``;
+    }
 
     return (
         <div>
-            <div className="chats">
-                {state.chats.map(chat => {
-                    return <p key={chat.timestamp}>{chat.content}</p>
+            <Header />
+            <div className="chat-area" ref={myRef}>
+                {/* loading indicator */}
+                {state.loadingChats ? <div className="spinner-border text-success" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div> : ""}
+                {/* chat area */}
+                {state.chats && state.chats.map(chat => {
+                    return <p key={chat.timestamp} className={"chat-bubble " + (state.user.uid === chat.uid ? "current-user" : "")}>
+                    {chat.content}
+                    <br />
+                    <span className="chat-time float-right">{formatTime(chat.timestamp)}</span>
+                    </p>
                 })}
+                </div>
+                <form onSubmit={handleSubmit} className="mx-3">
+                <textarea className="form-control" name="content" onChange={handleChange} value={state.content}></textarea>
+                {state.error ? <p className="text-danger">{state.error}</p> : null}
+                <button type="submit" className="btn btn-submit px-5 mt-4">Send</button>
+                </form>
+                <div className="py-5 mx-3">
+                Login in as: <strong className="text-info">{state.user.email}</strong>
             </div>
-            <div>
-                Login in as: <strong>{state.user.email}</strong>
-            </div>
-        </div>
+      </div>
     );
 }
